364955106805477cf2992e9f3cf55bf86b88fce7
imfly <kubying@foxmail.com>
2016-04-06 16:49:44 +0800

    Initial commit.

---
+++ addon/components/animated-header.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+import Scroller from 'ember-cli-scroller';
+
+var $document = Ember.$(document),
+  $window = Ember.$(window),
+  changeHeaderOn = 260;
+
+function scrollPage() {
+  var sy = scrollY();
+  if (sy >= changeHeaderOn) {
+    Ember.$('.cbp-af-header').addClass('cbp-af-header-shrink');
+  } else {
+    Ember.$('.cbp-af-header').removeClass('cbp-af-header-shrink');
+  }
+}
+
+function scrollY() {
+  return $window.pageYOffset || $document.scrollTop()
+}
+
+export default Ember.Component.extend(Scroller, {
+  didInsertElement: function() {
+    this.bindScrolling();
+  },
+
+  willRemoveElement: function() {
+    this.unbindScrolling();
+  },
+
+  scrolled: function() {
+    scrollPage();
+    console.log("ok");
+  }
+});
